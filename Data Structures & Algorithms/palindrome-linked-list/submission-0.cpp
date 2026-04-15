950fcdd03ab5dc0fc3ee4d36545e909a71be8173
WhaleFallBerial <87981608+WhaleFallBerial@users.noreply.github.com>
2026-04-15 11:22:36 +0800

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> arr;
+        ListNode *cur=head;
+        while(cur)
+        {
+            arr.push_back(cur->val);
+            cur=cur->next;
+        }
+
+        int l=0;
+        int r=arr.size()-1;
+        while(l<r)
+        {
+            if(arr[l]!=arr[r])
+            {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
