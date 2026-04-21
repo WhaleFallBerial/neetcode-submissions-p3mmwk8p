3784a054c3f20c04393f55df26bd7fd7ad6c8296
WhaleFallBerial <87981608+WhaleFallBerial@users.noreply.github.com>
2026-04-21 14:30:24 +0800

    Add: merge-two-binary-trees - submission-1

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-1.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==nullptr && root2==nullptr) return nullptr;
+        TreeNode* res=new TreeNode((root1?root1->val:0)+(root2?root2->val:0));
+        res->left=mergeTrees(root1?root1->left:nullptr, root2?root2->left:nullptr);
+        res->right=mergeTrees(root1?root1->right:nullptr, root2?root2->right:nullptr);
+        return res;
+    }
+
+};
