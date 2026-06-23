3fa816c5064dfaf823f0283539913f2690e82147
WhaleFallBerial <87981608+WhaleFallBerial@users.noreply.github.com>
2026-06-23 10:02:49 +0800

    Add: spiral-matrix-ii - submission-0

---
+++ Data Structures & Algorithms/spiral-matrix-ii/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        int left=0, right=n-1, top=0, bottom=n-1, val=1;
+        while(left<=right&&top<=bottom)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                res[top][i]=val;
+                val++;
+            }
+            top++;
+
+            for(int i=top;i<=bottom;i++)
+            {
+                res[i][right]=val;
+                val++;
+            }
+            right--;
+
+            for(int i=right;i>=left;i--)
+            {
+                res[bottom][i]=val;
+                val++;
+            }
+            bottom--;
+
+            for(int i=bottom;i>=top;i--)
+            {
+                res[i][left]=val;
+                val++;
+            }
+            left++;
+        }
+        return res;
+    }
+};
